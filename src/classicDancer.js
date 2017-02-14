dfd7d4a2fafe23a94da46eaa5fb975e40f3fe0ab
whitzhu <whitzhu@gmail.com>
2017-02-14 15:04:40 -0800

    add slide method for ClassicDancer and jump method for MinionDancer

--- src/classicDancer.js
+++ src/classicDancer.js
@@ -1,13 +1,9 @@
 var ClassicDancer = function(top, left, timeBetweenSteps) {
-  BlinkyDancer.call(this, top, left, timeBetweenSteps);
-  this.size = 10; 
-  this.setSize(this.size);
-  // this.speed = 'slow';
-  // this.gender = 'male';
+  Dancer.call(this, top, left, timeBetweenSteps);
+  this.$node = $('<span class="beyonce"><img src="https://31.media.tumblr.com/ce27425fa583c8f73d98ca9dc56716d6/tumblr_inline_n4i9fiD9LB1rr8huq.gif"></span>');
 };
 
-
-ClassicDancer.prototype = Object.create(BlinkyDancer.prototype);
+ClassicDancer.prototype = Object.create(Dancer.prototype);
 ClassicDancer.prototype.constructor = ClassicDancer;
 
 // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
@@ -15,16 +11,22 @@ ClassicDancer.prototype.constructor = ClassicDancer;
 ClassicDancer.prototype.step = function() {
   // call the old version of step at the beginning of any call to this new version of step
   Dancer.prototype.step.call(this);
+  this.slide();
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
   // this.$node.toggle();
-  this.size += 10;
 };
 
-ClassicDancer.prototype.setSize = function(size) { 
-  var styleSettings = {
-    size: size; //add a + 'px';
-  };
-  this.$node.css(styleSettings);
+ClassicDancer.prototype.slide = function() {
+  this.$node.animate({left: '+=300'}, 2000);
+  this.$node.animate({left: '-=300'}, 1000);
 };
+
+
+// ClassicDancer.prototype.setSize = function(size) { 
+//   var styleSettings = {
+//     size: size; //add a + 'px';
+//   };
+//   this.$node.css(styleSettings);
+// };
--- src/minionDancer.js
+++ src/minionDancer.js
@@ -12,8 +12,16 @@ MinionDancer.prototype.constructor = MinionDancer;
 MinionDancer.prototype.step = function() {
   // call the old version of step at the beginning of any call to this new version of step
   Dancer.prototype.step.call(this);
+  this.slide();
+  this.jump();
+  //ClassicDancer.prototype.slide.call(this);
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
   //this.$node.toggle();
+};
+
+MinionDancer.prototype.jump = function() {
+  this.$node.animate({top: '+=300'}, 2000);
+  this.$node.animate({top: '-=300'}, 1000);
 };
